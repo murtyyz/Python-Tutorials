5f1de1b7a176d5a263b71408f201317e0daf8ac8
murtyyz <murtyyz@outlook.com>
2017-07-17 19:18:53 +0100

    Add files via upload

---
+++ Favourite quote.py
@@ -0,0 +1,7 @@
+# Print favourite quote
+# Credit author
+
+print ("A Lion does not concern himself with the opinion of Sheep.")
+print ("credit: Tywin Lannister")
+input("\n\nPress the enter key to exit.")
+
---
+++ Prints name.py
@@ -0,0 +1,7 @@
+# Prints my name
+# User has to press enter key to exit
+
+print ("My name is Murtaza Zavery")
+
+input ("\n\nPress the enter key to exit.")
+
